824286f74862b90b32067f4e6778fef8220cf55a
OlexandrBuhaienko <soulofdeveloper0911@gmail.com>
2023-12-10 02:48:41 +0200

    Created environment file using terminal and command ng generate. Added the apiBaseUrl variable for this file as well as a class. And made the replacement from the category service while we was using the exact link, so now we use the updated string constant including apiBaseUrl variable and the exact path for the component in API as a endpoint

--- src/app/features/category/services/category.service.ts
+++ src/app/features/category/services/category.service.ts
@@ -2,7 +2,8 @@ import { Injectable } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-request-model';
 import { Observable } from 'rxjs';
 import { HttpClient } from '@angular/common/http';
-import { Category } from '../models/Category.model';
+import { Category } from '../models/category.model';
+import { environment } from 'src/environments/environment.development';
 @Injectable({
   providedIn: 'root'
 })
@@ -10,9 +11,9 @@ export class CategoryService {
  
   constructor(private http: HttpClient) {}
   addCategory(model:AddCategoryRequest): Observable<void>{
-    return this.http.post<void>('https://localhost:7081/api/categories', model);
+    return this.http.post<void>(`${environment.apiBaseUrl}/api/categories`, model);
   }
   getAllCategories() : Observable<Category[]>{
-    return this.http.get<Category[]>('https://localhost:7081/api/Categories')
+    return this.http.get<Category[]>(`${environment.apiBaseUrl}/api/Categories`)
   }
 }
