f9c8b9065a03402e29718c0c653d9f0d125b1dde
OlexandrBuhaienko <soulofdeveloper0911@gmail.com>
2023-12-19 02:27:01 +0200

    In the category-list.component.ts file, we removed the subscription on the observable and added the variable which is collecting the Observable<Category[]> type. Also, we added the value assigning for this variable, and the value is the Observable, which we are getting from the categoryService. In the category-list.component.html file, we switched the categories as a default ng value, for the categories$ | async as category temporary variable, which is using inside the *ngFor construction for looping throught the category list. Also, we made a change inside the add-category.component.ts file in onSubmit form and inside the block, where we showed before in console.log the successful response for the request. Here, we are just added here the route for the category list component after the category adding to the Db

--- src/app/features/category/add-category/add-category.component.ts
+++ src/app/features/category/add-category/add-category.component.ts
@@ -2,6 +2,7 @@ import { Observable, Subscription } from 'rxjs';
 import { CategoryService } from './../services/category.service';
 import { Component, OnDestroy } from '@angular/core';
 import { AddCategoryRequest } from '../models/add-category-request-model';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-add-category',
@@ -12,7 +13,7 @@ export class AddCategoryComponent implements OnDestroy {
 
   model: AddCategoryRequest;
   private addCategorySubscription ?:Subscription;
-  constructor (private CategoryService:CategoryService) {
+  constructor (private CategoryService:CategoryService, private router : Router) {
     this.model = {
       Name: '',
       UrlHandle: ''
@@ -27,7 +28,8 @@ onFormSubmit(){
   this.addCategorySubscription = this.CategoryService.addCategory(this.model)
   .subscribe({
     next: (response) => {
-      console.log('This was successful!');
+      console.log('Request was done successfully!');
+      this.router.navigateByUrl('/admin/categories');
     }, 
     error: (error) => {
       console.error('Bad request!');
