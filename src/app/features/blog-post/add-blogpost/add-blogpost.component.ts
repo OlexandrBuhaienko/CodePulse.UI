85f1855ac49b5b480294f3a75559026fdd09d117
OlexandrBuhaienko <soulofdeveloper0911@gmail.com>
2024-01-28 20:28:00 +0200

    We've added the two way data binding using our model we created for blog-post. Also, we made a changes to the add-blogpost.component.ts file, so we created the constructor inside the class and initialized all properties. Afer that, we made a method onFormSubmit here and assigned this to the form in html file. While form is submitting, we are showing the model we created and filled by ourselves

--- src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
+++ src/app/features/blog-post/add-blogpost/add-blogpost.component.ts
@@ -1,10 +1,28 @@
 import { Component } from '@angular/core';
+import { AddBlogPost } from '../models/add-blog-post.model';
+import { share } from 'rxjs';
 
 @Component({
   selector: 'app-add-blogpost',
   templateUrl: './add-blogpost.component.html',
-  styleUrls: ['./add-blogpost.component.css']
+  styleUrls: ['./add-blogpost.component.css'],
 })
 export class AddBlogpostComponent {
-
+  model: AddBlogPost;
+  constructor() {
+    this.model = {
+      title: '',
+      shortDescription: '',
+      urlHandle: '',
+      content: '',
+      featureImageUrl: '',
+      author: '',
+      isVisible: true,
+      pablishedDate: new Date()
+    }
+  }
+  onFormSubmit() : void{
+    console.log(this.model);
+  }
 }
+
