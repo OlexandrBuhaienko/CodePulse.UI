d9123af1f472da0497a6d40d8a2e569b43c1b3f9
OlexandrBuhaienko <soulofdeveloper0911@gmail.com>
2024-06-09 12:54:04 +0300

    I've implemented ngOnInit method and are getting an id inside of this to give that to our page, so we could showcase the id of the blogPost which was selected to edit. Additionally, I've implemented onDestroy interface and method

--- src/app/features/blog-post/edit-blogpost/edit-blogpost.component.ts
+++ src/app/features/blog-post/edit-blogpost/edit-blogpost.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
+import { Subscription } from 'rxjs';
 
 @Component({
   selector: 'app-edit-blogpost',
@@ -8,14 +9,21 @@ import { ActivatedRoute } from '@angular/router';
   templateUrl: './edit-blogpost.component.html',
   styleUrl: './edit-blogpost.component.css'
 })
-export class EditBlogpostComponent implements OnInit {
+export class EditBlogpostComponent implements OnInit, OnDestroy{
+  id: string | null = null;
+  routeSubscription?: Subscription;
   constructor(private route: ActivatedRoute){
 
   }
-
-
   ngOnInit(): void {
-    throw new Error('Method not implemented.');
+    this.routeSubscription = this.route.paramMap.subscribe({
+      next: (params) => {
+        this.id = params.get('id');
+      }
+    });
   }
 
+  ngOnDestroy(): void {
+    this.routeSubscription?.unsubscribe();
+  }
 }
