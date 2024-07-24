90fee3238e9097a09957f45c8d587e241c45a70b
OlexandrBuhaienko <soulofdeveloper0911@gmail.com>
2024-07-24 14:02:38 +0300

    Added a new component, called Home component, added implementation of onInit interface and created the template of the html, which we will use for displaying the BlogPosts at the home page. Additionally, added one more path option of the app.routing.module.ts file, to load our Home component when we are going to the empty path, or just clicking the home link at navbar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,36 +6,41 @@ import { EditCategoryComponent } from './features/category/edit-category/edit-ca
 import { BlogpostListComponent } from './features/blog-post/blogpost-list/blogpost-list.component';
 import { AddBlogpostComponent } from './features/blog-post/add-blogpost/add-blogpost.component';
 import { EditBlogpostComponent } from './features/blog-post/edit-blogpost/edit-blogpost.component';
+import { HomeComponent } from './features/public/home/home.component';
 
 const routes: Routes = [
-{
-  path: 'admin/categories',
-  component: CategoryListComponent
-},
-{
-  path: 'admin/categories/add',
-  component: AddCategoryComponent
-}, 
-{
-  path: 'admin/categories/:id',
-  component: EditCategoryComponent
-},
-{
-  path: 'admin/blogposts',
-  component: BlogpostListComponent
-}, 
-{
-  path: 'admin/blogposts/add',
-  component: AddBlogpostComponent
-}, 
-{
-  path: 'admin/blogposts/:id',
-  component: EditBlogpostComponent
-}
+  {
+    path: '',
+    component: HomeComponent,
+  },
+  {
+    path: 'admin/categories',
+    component: CategoryListComponent,
+  },
+  {
+    path: 'admin/categories/add',
+    component: AddCategoryComponent,
+  },
+  {
+    path: 'admin/categories/:id',
+    component: EditCategoryComponent,
+  },
+  {
+    path: 'admin/blogposts',
+    component: BlogpostListComponent,
+  },
+  {
+    path: 'admin/blogposts/add',
+    component: AddBlogpostComponent,
+  },
+  {
+    path: 'admin/blogposts/:id',
+    component: EditBlogpostComponent,
+  },
 ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
